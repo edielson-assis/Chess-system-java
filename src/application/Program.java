98da608c377aa94fc9cf4fbb6f5a6acaa5919d39
Edielson Assis <edielsonnice@hotmail.com>
2022-07-11 10:12:47 -0300

    Starting to implement Board and Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import boardGame.Board;
+
 public class Program {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Board board = new Board(8, 8);
     }
 }
